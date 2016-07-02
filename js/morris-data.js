79e0a7b2ae49a0184063d1a1f509b33f67214a98
Dexter JU <jvdajd@gmail.com>
2016-07-02 23:54:38 +0200

    Done

--- js/morris-data.js
+++ js/morris-data.js
@@ -3,59 +3,121 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2016 Q1',
+            Pipeline1: 35,
+            Pipeline2: 45,
+            Pipeline3: 45
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2015 Q2',
+            Pipeline1: 36,
+            Pipeline2: 47,
+            Pipeline3: 59
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2015 Q3',
+            Pipeline1: 21,
+            Pipeline2: 34,
+            Pipeline3: 56
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2015 Q4',
+            Pipeline1: 12,
+            Pipeline2: 43,
+            Pipeline3: 56
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2014 Q1',
+            Pipeline1: 21,
+            Pipeline2: 45,
+            Pipeline3: 54
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2014 Q2',
+            Pipeline1: 34,
+            Pipeline2: 45,
+            Pipeline3: 45
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2014 Q3',
+            Pipeline1: 23,
+            Pipeline2: 45,
+            Pipeline3: 73
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2014 Q4',
+            Pipeline1: 12,
+            Pipeline2: 34,
+            Pipeline3: 56
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2013 Q1',
+            Pipeline1: 21,
+            Pipeline2: 56,
+            Pipeline3: 32
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2013 Q2',
+            Pipeline1: 21,
+            Pipeline2: 34,
+            Pipeline3: 56
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['Pipeline1', 'Pipeline2', 'Pipeline3'],
+        labels: ['Pipeline1', 'Pipeline2', 'Pipeline3'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+
+
+    Morris.Area({
+        element: 'morris-area-chart-3',
+        data: [{
+            period: '2016 Q1',
+            Pipeline1: 35,
+            Pipeline2: 45,
+            Pipeline3: 45
+        }, {
+            period: '2015 Q2',
+            Pipeline1: 36,
+            Pipeline2: 47,
+            Pipeline3: 59
+        }, {
+            period: '2015 Q3',
+            Pipeline1: 21,
+            Pipeline2: 34,
+            Pipeline3: 56
+        }, {
+            period: '2015 Q4',
+            Pipeline1: 12,
+            Pipeline2: 43,
+            Pipeline3: 56
+        }, {
+            period: '2014 Q1',
+            Pipeline1: 21,
+            Pipeline2: 45,
+            Pipeline3: 54
+        }, {
+            period: '2014 Q2',
+            Pipeline1: 34,
+            Pipeline2: 45,
+            Pipeline3: 45
+        }, {
+            period: '2014 Q3',
+            Pipeline1: 23,
+            Pipeline2: 45,
+            Pipeline3: 73
+        }, {
+            period: '2014 Q4',
+            Pipeline1: 12,
+            Pipeline2: 34,
+            Pipeline3: 56
+        }, {
+            period: '2013 Q1',
+            Pipeline1: 21,
+            Pipeline2: 56,
+            Pipeline3: 32
+        }, {
+            period: '2013 Q2',
+            Pipeline1: 21,
+            Pipeline2: 34,
+            Pipeline3: 56
+        }],
+        xkey: 'period',
+        ykeys: ['Pipeline1', 'Pipeline2', 'Pipeline3'],
+        labels: ['Pipeline1', 'Pipeline2', 'Pipeline3'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
